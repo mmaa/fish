d7a43acc40e42f3ddc41384353837ad8880ec14d
Marcos Acosta <marcos@acosta.tv>
2013-06-17 19:28:56 -0500

    sublime, git alias

--- config.fish
+++ config.fish
@@ -1,19 +1,22 @@
 set -g -x fish_greeting ''
 
+function yt
+  screen -dm youtube-dl -f 22 -o "$HOME/Desktop/%(title)s.%(id)s.%(ext)s" $argv
+end
 function bu
   if count $argv >/dev/null
-    brew update
-    brew outdated
-  else
     brew upgrade $argv; and brew cleanup -f
+  else
+    brew update; and brew outdated
   end
 end
+function m;   subl .                            ; end
 function gb;  git branch $argv                  ; end
 function gc;  git checkout $argv                ; end
 function gd;  git diff head $argv               ; end
 function gdi; gd --ignore-space-change $argv    ; end
 function gdt; git difftool head $argv           ; end
-function gg;  git add .; and git add -u; and gs ; end
+function gg;  git add -A .; and gs              ; end
 function gm;  git commit $argv                  ; end
 function gmm; git commit -m $argv               ; end
 function gpl; git pull --prune                  ; end
@@ -25,7 +28,7 @@ function la;  ls -F -1 -A $argv                 ; end
 function ll;  ls -F -A -o -h $argv              ; end
 function tm;  tmux attach; or tmux -u           ; end
 
-set -g -x EDITOR 'vim'
+set -g -x EDITOR 'subl'
 set -g -x PAGER 'vimpager'
 set -g -x PGDATA '/usr/local/var/postgres/'
 
