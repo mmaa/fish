c198db9558f33bb3cc4574e1ee4e30ef65fde40e
Marcos Acosta <marcos@acosta.tv>
2013-07-11 18:43:35 -0500

    remove vimpager, aliases

--- config.fish
+++ config.fish
@@ -10,26 +10,25 @@ function bu
     brew update; and brew outdated
   end
 end
-function m;   subl .                            ; end
-function gb;  git branch $argv                  ; end
-function gc;  git checkout $argv                ; end
-function gd;  git diff head $argv               ; end
-function gdi; gd --ignore-space-change $argv    ; end
-function gdt; git difftool head $argv           ; end
-function gg;  git add -A .; and gs              ; end
-function gm;  git commit $argv                  ; end
-function gmm; git commit -m $argv               ; end
-function gpl; git pull --prune                  ; end
-function gps; git push $argv                    ; end
-function gpz; gpl; and gps                      ; end
-function gs;  git status                        ; end
-function l;   ls -F -1 $argv                    ; end
-function la;  ls -F -1 -A $argv                 ; end
-function ll;  ls -F -A -o -h $argv              ; end
-function tm;  tmux attach; or tmux -u           ; end
+
+function gb;  git branch $argv                                  ; end
+function gc;  git checkout $argv                                ; end
+function gd;  git diff head $argv | tig                         ; end
+function gdi; git diff head --ignore-space-change $argv | tig   ; end
+function gdt; git difftool head $argv                           ; end
+function gg;  git add -A .; and gs                              ; end
+function gm;  git commit $argv                                  ; end
+function gmm; git commit -m $argv                               ; end
+function gpl; git pull --prune                                  ; end
+function gps; git push $argv                                    ; end
+function gpz; gpl; and gps                                      ; end
+function gs;  git status                                        ; end
+function l;   ls -F -1 $argv                                    ; end
+function la;  ls -F -1 -A $argv                                 ; end
+function ll;  ls -F -A -o -h $argv                              ; end
+function tm;  tmux attach; or tmux -u                           ; end
 
 set -g -x EDITOR 'subl'
-set -g -x PAGER 'vimpager'
 set -g -x PGDATA '/usr/local/var/postgres/'
 
 set -g -x PATH /usr/local/bin /usr/local/sbin $PATH
